be6142e31ba3cfc73bcded581e77da4739011f0e
congnc2408 <congnc2408@gmail.com>
2024-09-09 00:36:38 +0700

    nodejs connection db test

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,16 @@
+const connection = require('../config/database')
+
 const getHomepage = (req, res) => {
-    res.send('Hello world!!');
+    let users = []
+    connection.query(
+        'select * from Users u',
+        function (err, results, fields) {
+            users = results
+            // console.log("field= ", fields);
+            res.send(JSON.stringify(users));
+        }
+    )
+
 }
 const getHoiCong = (req, res) => {
     res.render('sample.ejs')
--- src/server.js
+++ src/server.js
@@ -1,17 +1,31 @@
+require('dotenv').config();
 const express = require('express');
 const configViewEngine = require('./config/viewEngine');
 const app = express();
-require('dotenv').config();
 const webRoutes = require('./routes/web');
+const connection = require('./config/database')
 
 const port = process.env.PORT || 4200;
 const hostname = process.env.HOST_NAME;
 
-console.log(">>> check env: ", process.env);
+// console.log(">>> check env: ", process.env);
 //config template engine
 configViewEngine(app);
 
 app.use('/', webRoutes);
+
+
+
+connection.query(
+    'select * from Users u',
+    function (err, results, fields) {
+        console.log("result= ", results);
+        // console.log("field= ", fields);
+    }
+)
+
+
+
 app.listen(port, hostname, () => {
     console.log(`Exam app listening on ${hostname}:${port}`);
 });
